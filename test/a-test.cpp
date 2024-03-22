c7ad0a20f98beadcae09fce35b57f95091442a15
VinayKomaravolu <vinay.komaravolu@gmail.com>
2024-03-22 13:46:33 -0500

    Init

---
+++ test/a-test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <stdlib.h> /* srand, rand */
+
+#include <iostream>
+#include <random>
+#include <string>
+
+using namespace std;
+
+TEST(ATest, TestingA) { EXPECT_EQ(1, 1); }
---
+++ test/b-test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <stdlib.h> /* srand, rand */
+
+#include <iostream>
+#include <random>
+#include <string>
+
+using namespace std;
+
+TEST(BTest, TestingB) { EXPECT_EQ(1, 1); }
